ad73519e13b1a08b0fb7fefde6cb48496cccd2fc
bkirilll <kirya.bulekov@mail.ru>
2023-11-01 12:57:28 +0300

    Task 1 fixed

--- sem4/src/main/java/org/example/task1/Main.java
+++ sem4/src/main/java/org/example/task1/Main.java
@@ -2,26 +2,15 @@ package org.example.task1;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
 
         System.out.println(authorization("aboba", "1234ytrwtwetwetwetwwetwe", "1234"));
 
     }
 
-    public static boolean authorization(String login, String password, String confirmPassword) {
-        if(login.length() > 20) try {
-            throw new WrongLoginException(login);
-        } catch (WrongLoginException e) {
-            throw new RuntimeException(e);
-        }
-        if(password.length() > 20 || !password.equals(confirmPassword)) try {
-            throw new WrongPasswordException(password, confirmPassword);
-        } catch (WrongPasswordException e) {
-            throw new RuntimeException(e);
-        }
-        else {
-            return true;
-        }
-
+    public static boolean authorization(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if(login.length() > 20) throw new WrongLoginException(login);
+        if(password.length() > 20 || !password.equals(confirmPassword))  throw new WrongPasswordException(password, confirmPassword);
+        return true;
     }
 }
